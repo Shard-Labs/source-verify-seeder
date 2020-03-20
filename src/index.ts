dd61c5090abf54a234be28fd4616753b5d881834
Jakov Buratovic <kraljipo007@gmail.com>
2020-03-20 18:57:43 +0100

    basic dependencies and npm run scripts

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+
+const port = process.env.SERVER_PORT; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
