90b570c03a0b3f76acb0b8618cd28ebd87e1777f
Jakov Buratovic <kraljipo007@gmail.com>
2020-03-24 11:31:15 +0100

    error middleware

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,15 @@ dotenv.config();
 const port = process.env.SERVER_PORT; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+import errorMiddleware from "./middleware/errorMiddleware";
+app.use(errorMiddleware);
 
 // start the Express server
-app.listen( port, () => {
+app.listen(port, () => {
     // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+    console.log(`server started at http://localhost:${port}`);
+});
